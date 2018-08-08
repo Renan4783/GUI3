6f52b1aa428535d528e5c17e9b9a7416e30c303c
ranemj77 <renanmj77@gmail.com>
2018-08-08 18:01:33 -0300

    versao original

---
+++ src/Exercicio1/ComparaTexto.java
@@ -0,0 +1,18 @@
+package Exercicio1;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ComparaTexto {
+
+	JLabel jlab;
+	JButton jbtn;
+	JTextField jtx1;
+	JTextField jtx2;
+	
+	public ComparaTexto(){
+		JFrame jfrm = new JFrame("Comparador de textos");
+		
+	}
+}
